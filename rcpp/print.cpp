bb1e97fe6fd1e340bf5c9b0ee0ad748d6e91f1f1
Sean Anderson <sean@seananderson.ca>
2019-09-22 15:23:34 -0700

    Delete a space

--- rcpp/print.cpp
+++ rcpp/print.cpp
@@ -6,7 +6,7 @@ void rcpp_rcout(
   Rcpp::NumericVector v){
 
   // printing value of vector
-  Rcpp::Rcout << "The value of v : " << v << "\n";
+  Rcpp::Rcout << "The value of v: " << v << "\n";
 
   // printing error message
   Rcpp::Rcerr << "Error message\n";
